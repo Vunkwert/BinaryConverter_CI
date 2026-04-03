570c404555b0d39b71e5d221e0330691badef6f8
Svistunova Katya <svistuniva05@mail.ru>
2026-04-03 09:10:51 +0300

    Add GenCode2

--- src/Module.Core/BinaryToDecimalConverter.cs
+++ src/Module.Core/BinaryToDecimalConverter.cs
@@ -1,20 +1,31 @@
 using System;
+using System.Text;
 using Lab.Interfaces;
 
-namespace Lab.Implementations.GenCode1;
+namespace Lab.Implementations.GenCode2;
 
 public class BinaryToDecimalConverter : IBinaryToDecimalConverter
 {
     public string ToBinary(int decimalNumber)
     {
-        if (decimalNumber < 0) throw new ArgumentException("Число < 0");
-        return Convert.ToString(decimalNumber, 2);
+        if (decimalNumber < 0) throw new ArgumentException();
+        if (decimalNumber == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        while (decimalNumber > 0) {
+            sb.Insert(0, decimalNumber % 2);
+            decimalNumber /= 2;
+        }
+        return sb.ToString();
     }
 
     public int ToDecimal(string binaryString)
     {
-        if (string.IsNullOrEmpty(binaryString)) throw new ArgumentException("Пусто");
-        try { return Convert.ToInt32(binaryString, 2); }
-        catch { throw new ArgumentException("Ошибка формата"); }
+        if (binaryString == null || binaryString == "") throw new ArgumentException();
+        int res = 0;
+        foreach (char c in binaryString) {
+            if (c != '0' && c != '1') throw new ArgumentException();
+            res = (res << 1) + (c - '0');
+        }
+        return res;
     }
 }
