8d23e26411687381f49e8a9d8c281737cd92d006
Svistunova Katya <svistuniva05@mail.ru>
2026-04-03 09:12:44 +0300

    Add GenCode3

--- src/Module.Core/BinaryToDecimalConverter.cs
+++ src/Module.Core/BinaryToDecimalConverter.cs
@@ -1,31 +1,18 @@
 using System;
-using System.Text;
+using System.Linq;
 using Lab.Interfaces;
 
-namespace Lab.Implementations.GenCode2;
+namespace Lab.Implementations.GenCode3;
 
 public class BinaryToDecimalConverter : IBinaryToDecimalConverter
 {
-    public string ToBinary(int decimalNumber)
-    {
-        if (decimalNumber < 0) throw new ArgumentException();
-        if (decimalNumber == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-        while (decimalNumber > 0) {
-            sb.Insert(0, decimalNumber % 2);
-            decimalNumber /= 2;
-        }
-        return sb.ToString();
-    }
+    public string ToBinary(int decimalNumber) => 
+        decimalNumber >= 0 ? Convert.ToString(decimalNumber, 2) : throw new ArgumentException();
 
     public int ToDecimal(string binaryString)
     {
-        if (binaryString == null || binaryString == "") throw new ArgumentException();
-        int res = 0;
-        foreach (char c in binaryString) {
-            if (c != '0' && c != '1') throw new ArgumentException();
-            res = (res << 1) + (c - '0');
-        }
-        return res;
+        if (string.IsNullOrWhiteSpace(binaryString) || !binaryString.All(c => c == '0' || c == '1'))
+            throw new ArgumentException();
+        return Convert.ToInt32(binaryString, 2);
     }
 }
